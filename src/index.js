c9fc34f9cd0e722332fb2658f671d72efd4ddcdf
Gunwoo Ha <rjsdn827@gmail.com>
2021-08-31 10:07:14 +0900

    feat: 백엔드 Entry 포인트 생성
    - chalk를 이용한 콘솔로그 기능

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+
+const express = require('express') //import express.js
+const app = express()
+const port = 9090
+
+app.get('/', (request, response) =>{
+    response.send("Hello Molto!")
+})
+
+app.listen(port, ()=>{
+
+    serverLog("success", port)
+})
+
+
+
+const serverLog = (status, port) =>{
+    const chalk = require('chalk')
+    if(status == "success")
+        console.log(chalk.bold('[Molto]:') + ' 백엔드 서버가 성공적으로 구동되었습니다! '+chalk.yellowBright(`(포트:${port})`))
+
+}
