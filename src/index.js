2e3a854e3f722c5de5eca484500a89bc226840e2
HARDTACK-Dev <rjsdn827@gmail.com>
2021-09-23 22:03:05 +0900

    feat: 내용 반환하는 함수 구현

--- src/index.js
+++ src/index.js
@@ -14,25 +14,57 @@ const db = mysql.createConnection({
 
 db.connect();
 
-const title = "강남스타일"
-const artist = "PSY"
+// const title = "강남스타일"
+// const artist = "lol"
 
 
-db.query(`SELECT * from Music WHERE title='${title}' AND artist='${artist}'`, (error, rows, fields) =>{
-    if(error) throw error;
-    console.log('Music info is: ', rows);
+
+app.get('/specific/:topicQuery/:typeQuery', (request, response) =>{
+    //아티스트(artist)와 노래(title) 나누기
+    const topicDivided = request.params.topicQuery.split(':')
+
+    const title = topicDivided[0]
+    const artist = topicDivided[1]
+    var type;
+
+    switch(request.params.typeQuery){
+        case 'intro':
+            type = 'content_intro';
+            break;
+        case 'lyrics':
+            type = 'content_lyrics';
+            break;
+        case 'info':
+            type = 'content_info';
+            break
+        case 'etc':
+            type = 'content_etc';
+            break;
+        case 'relate':
+            type = 'content_relate';
+            break;
+    }
+
+    db.query(`SELECT ${type} from Music WHERE title='${title}' AND artist='${artist}'`, (error, rows, fields) =>{
+        if(error) throw error;
+    
+        if(rows == "")
+            console.log("비었다!")
+        
+        console.log('Music info is: ', rows);
+        response.send(rows)
+    })
+    
+    
 })
 
-db.end();
 
 
-app.get('/', (request, response) =>{
-    response.send("Hello Molto!")
-})
 
 app.listen(port, ()=>{
     serverLog("success", port)
 })
+
 
 
 const serverLog = (status, port) =>{
