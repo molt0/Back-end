3f086e279e708ae64dbc411af3eb00f31c1a7070
Gunwoo Ha <rjsdn827@gmail.com>
2021-10-08 10:20:55 +0900

    add / fix: 기능 Merge밑 정렬

--- src/index.js
+++ src/index.js
@@ -1,96 +1,151 @@
-const path = require('path')
-const express = require('express') //import express.js
-const ejs = require('ejs');
-const uuid4 = require('uuid4');
-var serveStatic = require('serve-static');      //특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 열어주는 역할
-var cookieParser = require('cookie-parser');
-var expressSession = require('express-session');
-var cors = require('cors');
+const path = require("path");
+const express = require("express"); //import express.js
+const ejs = require("ejs");
+const uuid4 = require("uuid4");
+const chalk = require("chalk");
+var serveStatic = require("serve-static"); //특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 열어주는 역할
+var cookieParser = require("cookie-parser");
+var expressSession = require("express-session");
+var cors = require("cors");
 
-const app = express()
-const port = 1004
+const app = express();
+const port = 1004;
 
 app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const serverLog = (status, port) =>{
-    const chalk = require('chalk')
-    if(status == "success")
-        console.log(chalk.bold('[Molto]:') + ' 백엔드 서버가 성공적으로 구동되었습니다! '+chalk.yellowBright(`(포트:${port})`))
-}
+let serverState = {
+  dbConnected: true,
+  port: port,
+};
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const con = mysql.createConnection({
-    host: 'test.itsw.info',
-    port: '3306',
-    user: 'sw202134',
-    password: 'sw202134',
-    database: 'sw202134'
+  host: "test.itsw.info",
+  port: "3306",
+  user: "sw202134",
+  password: "sw202134",
+  database: "sw202134",
 });
 // sw202134
 
 //////////////////////////////////////////////////////////////////////////////
-con.connect(function(err) {
-    if(err) {
-        res.send(err)
-        return;
-    };
-    console.log('Connected');
+con.connect(function (err) {
+  if (err) {
+    res.send(err);
+    return;
+  }
+
+  serverState.dbConnected = true;
 });
 
 //리스트
-app.get('/userinfo', (req, res) =>{
-    const sql = 'select * from users'
-    con.query(sql, function (err, result, fields) {
-        if(err) {
-            res.send(err)
-            return;
-        };
-        res.render('index',{user: result});
-    });
+app.get("/userinfo", (req, res) => {
+  const sql = "select * from users";
+  con.query(sql, function (err, result, fields) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    res.render("index", { user: result });
+  });
 });
 
 //가입
-app.post('/add',(req, res) =>{
-
-    req.body.uuid = uuid4();
-    console.log(req.body);
-    const sql = "INSERT INTO users SET ?"
-    con.query(sql, req.body, function(err, result, fields){
-        if(err) {
-            res.send(err)
-            return;
-        };
-        console.log("등록 완료");
-        // res.send('등록이 완료 되었습니다.');
-        res.redirect('/login')
-    });
+app.post("/add", (req, res) => {
+  req.body.uuid = uuid4();
+  console.log(req.body);
+  const sql = "INSERT INTO users SET ?";
+  con.query(sql, req.body, function (err, result, fields) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    console.log("등록 완료");
+    // res.send('등록이 완료 되었습니다.');
+    res.redirect("/login");
+  });
 });
 
+app.post("/checkid", function (req, res) {
+  let user_id = req.body.id;
 
-app.post('/checkid', function (req, res) { 
-    let user_id = req.body.id;     
-  
-    console.log(req.body.id);
-    let sql = 'select user_id from users where user_id=?' 
-    con.query(sql, [user_id], function (err, rows, fields) {
-        console.log(rows);
-        let checkid = new Object();
-        checkid.tf =false;             
-  
-        if (rows[0] === undefined) { 
-            checkid.tf = true; 
-            res.send(checkid);  
-        }else {
-            checkid.tf = false; 
-            res.send(checkid);  
-        }
-    })
+  console.log(req.body.id);
+  let sql = "select user_id from users where user_id=?";
+  con.query(sql, [user_id], function (err, rows, fields) {
+    console.log(rows);
+    let checkid = new Object();
+    checkid.tf = false;
+
+    if (rows[0] === undefined) {
+      checkid.tf = true;
+      res.send(checkid);
+    } else {
+      checkid.tf = false;
+      res.send(checkid);
+    }
+  });
 });
+
+//내용 Get으로 불러올 떄
+app.get("/specific/:title/:artist/:typeQuery", (request, response) => {
+  //아티스트(artist)와 노래(title) 나누기
+
+  const title = request.params.title;
+  const artist = request.params.artist;
+  var type;
+
+  switch (request.params.typeQuery) {
+    case "intro":
+      type = "content_intro";
+      break;
+    case "lyrics":
+      type = "content_lyrics";
+      break;
+    case "info":
+      type = "content_info";
+      break;
+    case "etc":
+      type = "content_etc";
+      break;
+    case "relate":
+      type = "content_relate";
+      break;
+    default:
+      response.sendStatus(404, "알맞은 Type이 아닙니다");
+      return;
+  }
+
+  db.query(
+    `SELECT ${type} from Music WHERE title='${title}' AND artist='${artist}'`,
+    (error, rows, fields) => {
+      if (error) throw error;
+
+      if (rows == "") {
+        result = { title: title, artist: artist, type: type, content: false };
+        response.send(JSON.stringify(result));
+        return;
+      }
+      console.log("비었다!");
+
+      result = { title: title, artist: artist, type: type, content: rows };
+      response.send(JSON.stringify(result));
+
+      console.log("Music info is: ", result);
+    }
+  );
+});
+
+// 내용이  POST로 왔을 때
+app.post("/specific/:title/:artist/:typeQuery", (request, response) => {
+  //save content!
+});
+
+// 최근 리스트
 
 // //업데이트 폼
 // app.get('/edit/:user_id', (req,res)=>{
@@ -126,10 +181,26 @@ app.post('/checkid', function (req, res) {
 //             return;
 //         };
 //         console.log(result)
-//         res.redirect('/userinfo');  
+//         res.redirect('/userinfo');
 //     });
 // });
 
-app.listen(port, ()=>{
-    serverLog("success", port)
-})
+const serverLog = (runState, dbState, port) => {
+  if (dbState == true)
+    console.log(
+      chalk.bold("[Molto]: ") +
+        "백엔드 MySQL 데이터베이스 연결 - " +
+        chalk.yellowBright(`(연결됨)`)
+    );
+
+  if (runState == "success")
+    console.log(
+      chalk.bold("[Molto]:") +
+        " 백엔드 코드가 오류없이 구동되었습니다! " +
+        chalk.yellowBright(`(포트:${port})`)
+    );
+};
+
+app.listen(port, () => {
+  serverLog("success", serverState.dbConnected, serverState.port);
+});
