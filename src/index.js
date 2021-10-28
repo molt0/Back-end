bd869b2375e3f80bf59c80198a16848d957ccbff
HARDTACK-Dev <rjsdn827@gmail.com>
2021-10-28 22:16:14 +0900

    chore: Prettier 정렬

--- src/index.js
+++ src/index.js
@@ -1,56 +1,48 @@
-const path = require("path");
 const express = require("express"); //import express.js
-const ejs = require("ejs");
-const uuid4 = require("uuid4");
 const chalk = require("chalk");
+const mysql = require("mysql2");
+const uuid4 = require("uuid4");
+const cors = require("cors");
+
+//플러그인
 const dateTime = require("node-datetime");
-var serveStatic = require("serve-static"); //특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 열어주는 역할
-var cookieParser = require("cookie-parser");
-var expressSession = require("express-session");
-var cors = require("cors");
 
 const app = express();
 const port = 1004;
 
-
-app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 app.use(cors());
+
 app.set("view engine", "ejs");
 
-let serverState = {
-  dbConnected: true,
+let status = {
+  DBConnected: true,
   port: port,
 };
 
-const mysql = require("mysql2");
-
-const con = mysql.createConnection({
+// DB 정보
+const db = mysql.createConnection({
   host: "localhost",
   port: "9999",
   user: "root",
   password: "test",
   database: "Molto",
 });
-// sw202134
 
-//////////////////////////////////////////////////////////////////////////////
-con.connect(function (err) {
+// DB 연결 테스트
+db.connect(function (err) {
   if (err) {
     res.send(err);
     return;
   }
-
-  serverState.dbConnected = true;
+  status.DBConnected = true;
 });
 
 //리스트
 app.get("/userinfo", (req, res) => {
   const sql = "select * from users";
-  con.query(sql, function (err, result, fields) {
+  db.query(sql, function (err, result, fields) {
     if (err) {
       res.send(err);
       return;
@@ -64,7 +56,7 @@ app.post("/add", (req, res) => {
   req.body.uuid = uuid4();
   console.log(req.body);
   const sql = "INSERT INTO users SET ?";
-  con.query(sql, req.body, function (err, result, fields) {
+  db.query(sql, req.body, function (err, result, fields) {
     if (err) {
       res.send(err);
       return;
@@ -78,7 +70,7 @@ app.post("/checkid", function (req, res) {
 
   console.log(req.body.id);
   let sql = "select user_id from users where user_id=?";
-  con.query(sql, [user_id], function (err, rows, fields) {
+  db.query(sql, [user_id], function (err, rows, fields) {
     console.log(rows);
     let checkid = new Object();
     checkid.tf = false;
@@ -93,15 +85,15 @@ app.post("/checkid", function (req, res) {
   });
 });
 
-//내용 Get으로 불러올 떄
+// 문서 내용 불러오기
 app.get("/specific/:title/:artist/:typeQuery", (request, response) => {
-  //아티스트(artist)와 노래(title) 나누기
+ 
 
   const title = request.params.title;
   const artist = request.params.artist;
   const type = request.params.typeQuery;
 
-  con.query(
+  db.query(
     `SELECT ${type} from Music WHERE title='${title}' AND artist='${artist}'`,
     (error, rows, fields) => {
       if (error) throw error;
@@ -109,13 +101,12 @@ app.get("/specific/:title/:artist/:typeQuery", (request, response) => {
       if (rows == "") {
         result = { title: title, artist: artist, type: type, contents: false };
         response.send(JSON.stringify(result));
-        console.log("데이터 없음")
+        console.log("데이터 없음");
         return;
       }
 
       result = { title: title, artist: artist, type: type, contents: rows[0] };
       response.send(JSON.stringify(result));
-
       console.log("Music info is: ", result);
     }
   );
@@ -123,91 +114,74 @@ app.get("/specific/:title/:artist/:typeQuery", (request, response) => {
 
 // 내용이  POST로 왔을 때
 app.post("/specific/:title/:artist/:typeQuery", (request, response) => {
-  //save content!
-
-	const title = request.params.title;
+  const title = request.params.title;
   const artist = request.params.artist;
   const type = request.params.typeQuery;
 
-  const body = JSON.stringify(request.body.savedContent)
+  const body = JSON.stringify(request.body.savedContent);
 
-  const defaultRow = '{"time": 1634822925666, "blocks": [{"id": "pYnFEuymaC", "data": {"text": "&nbsp;&nbsp;", "level": 2}, "type": "header"}], "version": "2.22.2"}'
+  const defaultRow =
+    '{"time": 1634822925666, "blocks": [{"id": "pYnFEuymaC", "data": {"text": "&nbsp;&nbsp;", "level": 2}, "type": "header"}], "version": "2.22.2"}';
 
+  db.query(
+    `SELECT title FROM Music WHERE title = '${title}' AND artist = '${artist}'`,
+    (error, row, fields) => {
+      if (error) throw error;
+      console.log("POST 들어옴");
+      console.log(row);
 
-	con.query(`SELECT title FROM Music WHERE title = '${title}' AND artist = '${artist}'`, (error, row, fields)=>{
-		if (error) throw error;
-    console.log("POST 들어옴")
-    console.log(row)
+      if (row.length === 0) {
+        console.log("POST - 내용이 존재하지 않음");
+        let dt = dateTime.create();
+        let date = dt.format("Y-m-d H:M:S");
+        db.query(
+          `INSERT INTO Music VALUES( 1, '${title}', '${artist}', '${body}', '${defaultRow}', '${defaultRow}', '${defaultRow}', '${defaultRow}', '${date}', '${date}')`,
+          (error, row, fields) => {
+            if (error) throw error;
+            console.log(row);
+            console.log("데이터가 생성됨");
 
-    if(row.length === 0){
-      console.log('POST - 내용이 존재하지 않음')
-      let dt = dateTime.create()
-      let date = dt.format('Y-m-d H:M:S')
-      con.query(`INSERT INTO Music VALUES( 1, '${title}', '${artist}', '${body}', '${defaultRow}', '${defaultRow}', '${defaultRow}', '${defaultRow}', '${date}', '${date}')`, (error, row, fields)=>{
-		    if (error) throw error;
-        console.log(row)
-        console.log("데이터가 생성됨")
+            return;
+          }
+        );
+      } else if (row[0].title === title) {
+        console.log("해당 제목을 가진 문서가 존재함");
+        //UPDATE문을 사용 (title과 artist가 params로 들어온거로)
+        db.query(
+          `UPDATE Music SET ${type} = '${body}' WHERE title = '${title}' AND artist = '${artist}'`,
+          (error, row) => {
+            if (error) throw error;
 
-        return;
-      })
-    }else if(row[0].title === title){
-			console.log("해당 제목을 가진 문서가 존재함")
-			//UPDATE문을 사용 (title과 artist가 params로 들어온거로)
-			con.query(`UPDATE Music SET ${type} = '${body}' WHERE title = '${title}' AND artist = '${artist}'`, (error, row) =>{
-				if(error) throw error
-
-				console.log('update: ' + row)
-			})
-		}
-
-
-    
-	})
-	
-
-
-
+            console.log("update: " + row);
+          }
+        );
+      }
+    }
+  );
 });
 
-// 최근 리스트
+//검색 기능
+app.get("/search/:query", (request, response) => {
+  const query = request.params.query;
 
-// //업데이트 폼
-// app.get('/edit/:user_id', (req,res)=>{
-//     const sql = "SELECT * FROM users WHERE user_id = ?";
-//     con.query(sql,[req.params.user_id],function(err,result,fields){
-//         if(err) {
-//             res.send(err)
-//             return;
-//         };
-//         res.render('edit',{user: result});
-//     });
-// });
+  db.query(
+    `SELECT title, artist FROM Music WHERE title = '${query}'`,
+    (error, row) => {}
+  );
+});
 
-// //업데이트
-// app.post('/update/:user_id', (req,res)=>{
-//     const sql = "UPDATE users SET ? WHERE user_id = " + req.params.user_id;
-//     con.query(sql,req.body,function(err,result,fields){
-//         if(err) {
-//             res.send(err)
-//             return;
-//         };
-//         console.log(result)
-//         res.redirect('/userinfo');
-//     });
-// });
-
- //삭제
- app.get('/delete/:user_id', (req,res)=>{
-     const sql = "DELETE FROM users WHERE user_id = ?";
-     con.query(sql,[req.params.user_id],function(err,result,fields){
-         if(err) {
-             res.send(err)
-             return;
-         };
-         console.log(result)
-         res.redirect('/userinfo');
-     });
- });
+//삭제
+app.get("/delete/:user_id", (req, res) => {
+  const sql = "DELETE FROM users WHERE user_id = ?";
+  db.query(sql, [req.params.user_id], function (err, result, fields) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    console.log(result);
+    res.redirect("/userinfo");
+  });
+});
 
 const serverLog = (runState, dbState, port) => {
   if (dbState == true)
@@ -226,5 +200,5 @@ const serverLog = (runState, dbState, port) => {
 };
 
 app.listen(port, () => {
-  serverLog("success", serverState.dbConnected, serverState.port);
+  serverLog("success", status.DBConnected, status.port);
 });
